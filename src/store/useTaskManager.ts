867ec5ca8d3e31fa6349db1496d70ba15e38a37c
Aina Rass <hei.aina.59@gmail.com>
2023-06-15 11:19:50 +0100

    feat:add task store

--- src/store/useTaskManager.ts
+++ src/store/useTaskManager.ts
@@ -1,7 +1,57 @@
-const useTaskManager = () => {
 
-}
+import create from 'zustand';
 
-export {
-  useTaskManager
-}
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+};
+
+type TaskManagerState = {
+  tasks: Task[];
+  searchTasks: (query: string) => void;
+  addTask: (title: string) => void;
+  updateTask: (id: string, updatedTask: Partial<Task>) => void;
+  deleteTask: (id: string) => void;
+};
+
+const useTaskManager = create<TaskManagerState>((set) => ({
+  tasks: [],
+  searchTasks: (query: string) => {
+    set((state) => ({
+      tasks: state.tasks.filter((task) =>
+        task.title.toLowerCase().includes(query.toLowerCase())
+      ),
+    }));
+  },
+  addTask: (title: string) => {
+    const newTask: Task = {
+      id: Date.now().toString(),
+      title,
+      completed: false,
+    };
+    set((state) => ({
+      tasks: [...state.tasks, newTask],
+    }));
+  },
+  updateTask: (id: string, updatedTask: Partial<Task>) => {
+    set((state) => ({
+      tasks: state.tasks.map((task) => {
+        if (task.id === id) {
+          return {
+            ...task,
+            ...updatedTask,
+          };
+        }
+        return task;
+      }),
+    }));
+  },
+  deleteTask: (id: string) => {
+    set((state) => ({
+      tasks: state.tasks.filter((task) => task.id !== id),
+    }));
+  },
+}));
+
+export default useTaskManager;
